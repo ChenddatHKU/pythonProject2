b705fcbb6df307926667b611fae9ecd610cb4b53
Chendd <chendd3038@gmail.com>
2022-01-30 10:59:28 +0800

    Initial commit

--- dissecting source codes.py
+++ dissecting source codes.py
@@ -1,22 +1,62 @@
-import scanpy as sc
-import numpy as np
-import pandas as pd
-help(sc.settings.verbosity)
+from Bio import AlignIO
+import os
+import glob
+from Bio import SeqIO
+
+def statistics (infile):
+    n = 0
+    list = []
+    list_nonL = []
+    align = AlignIO.read(infile, 'fasta')
+    for record in align:
+        n += 1
+        if len(record) < 716 or record[0] != 'M': continue
+        if record.seq[106] != 'X' and '-':
+            list.append(record.seq[106])
+            if record.seq[106] != 'L':
+                list_nonL.append(record.seq[106])
+                print(record, record.seq)
+    # print(len(list), n)
+    year = infile.rsplit('/')[6].rsplit('.')[0]
+    totalsequence = n
+    nonLlist = list_nonL
+    nonLPercentage  = len(list_nonL) / len(list) * 100
+
+    return [year, totalsequence, nonLlist, nonLPercentage]
+
+    # print("total sequence# = " + str(n))
+    # print("nonLlist =" + str(list_nonL))
+    # print("non L%" + " in" + ' ' + str(n) + "=" + str(len(list_nonL)/len(list) *100))
+
+outfile = open('/Users/chendd/Documents/HKU_Experiment/B Yamagata PA alignment/result', 'w')
+header = "\t".join(['year', 'totalsequence', 'nonLlist', 'nonLPercentage'])
+outfile.write(header + "\n")
+filenames = sorted(glob.glob('/Users/chendd/Documents/HKU_Experiment/B Yamagata PA alignment/*.fas')) #change the location of NGS raw data accordingly
+for filename in filenames:
+  print(filename)
+  fileID = filename.rsplit('/')[6].rsplit('.')[0]       # change the method to extract fileID according to the file name of your own.
+  result = statistics(filename)
+  print(result)
+  outfile.write(str(result[0]) + "\t" + str(result[1]) + "\t" + str(result[2]) + "\t" + str(result[3]) + "\n")
 
 
-# sc.settings.verbosity = 3             # verbosity: errors (0), warnings (1), info (2), hints (3)
-# sc.logging.print_header()
-# sc.settings.set_figure_params(dpi=80, facecolor='white')
-# results_file = 'write/pbmc3k.h5ad'  # the file that will store the analysis results
-# adata = sc.read_10x_mtx(
-#     'data/filtered_gene_bc_matrices/hg19/',  # the directory with the `.mtx` file
-#     var_names='gene_symbols',                # use gene symbols for the variable names (variables-axis index)
-#     cache=True)                              # write a cache file for faster subsequent reading
-# adata.var_names_make_unique()  # this is unnecessary if using `var_names='gene_ids'` in `sc.read_10x_mtx`
+# ##    if sth wrong, check the raw fas file, mainly check the location of L. sometimes "-" will be added when do multiple alignment, thus the location of L maybe changed accordingly depending on how many "-" added.
+# align = AlignIO.read('/Users/chendd/Documents/HKU_Experiment/PA alignment/190120-200720PA.fas', 'fasta')
+# n = 0
+# list = []
+# list_nonL = []
+# for record in align:
+#     print(record.seq, record.seq[106]) # change the number accordingly.
+#     n += 1
+#     if len(record) < 716 or record[0] != 'M': continue
+#     if record.seq[106] != 'X' and '-':
+#         list.append(record.seq[106])
+#         if record.seq[106] != 'L':
+#             list_nonL.append(record.seq[106])
+#             print(record, record.seq)
 #
-# adata
-# sc.pl.highest_expr_genes(adata, n_top=20, )
-# sc.pp.filter_cells(adata, min_genes=200)
-# sc.pp.filter_genes(adata, min_cells=3)
-# adata.var['mt'] = adata.var_names.str.startswith('MT-')  # annotate the group of mitochondrial genes as 'mt'
-# sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
+# totalsequence = n
+# nonLlist = list_nonL
+# nonLPercentage  = len(list_nonL) / len(list) * 100
+# print(totalsequence, nonLlist, nonLPercentage)
+
